29e1e77d301bae667059d2edb246f8616d82da9d
heromazinga <heromazinga@gmail.com>
2026-05-23 11:04:42 +0900

    chore: clean up config now that build trace issue is resolved

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/**',
-        'node_modules/@esbuild/**',
-        'node_modules/webpack/**',
-        'node_modules/sharp/**',
-        'node_modules/terser/**',
-        'node_modules/.cache/**',
-      ],
-    },
-  },
 };
 
 export default nextConfig;
